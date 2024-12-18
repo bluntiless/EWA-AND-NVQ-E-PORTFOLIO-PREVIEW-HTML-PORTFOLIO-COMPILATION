dbc5a142c404e971ec4dde1bf56792cf73abb47d
bluntiless <waynezilla@hotmail.co.uk>
2024-12-18 15:57:45 +0000

    Initial Commit

---
+++ EWA AND NVQ E PORTFOLIO/EWA_AND_NVQ_E_PORTFOLIOApp.swift
@@ -0,0 +1,17 @@
+//
+//  EWA_AND_NVQ_E_PORTFOLIOApp.swift
+//  EWA AND NVQ E PORTFOLIO
+//
+//  Created by WAYNE WRIGHT on 18/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct EWA_AND_NVQ_E_PORTFOLIOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EWA AND NVQ E PORTFOLIOTests/EWA_AND_NVQ_E_PORTFOLIOTests.swift
@@ -0,0 +1,17 @@
+//
+//  EWA_AND_NVQ_E_PORTFOLIOTests.swift
+//  EWA AND NVQ E PORTFOLIOTests
+//
+//  Created by WAYNE WRIGHT on 18/12/2024.
+//
+
+import Testing
+@testable import EWA_AND_NVQ_E_PORTFOLIO
+
+struct EWA_AND_NVQ_E_PORTFOLIOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
